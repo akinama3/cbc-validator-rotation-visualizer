5cb5c375a84b28fe599e750a1e6195c2b8cd8b71
Jun Katagiri <katagiri.jun2@gmail.com>
2019-04-08 13:32:14 +1000

    added YAML Dataloader button

---
+++ Unity/Assets/Scripts/YamlDataLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class YamlDataLoader : MonoBehaviour
+{
+    /// <summary>
+    /// YAMLファイルをロードするためのボタン
+    /// </summary>
+    [SerializeField] private Button loadButton;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    public void OnClickLoadButton()
+    {
+        Debug.Log("Clicked Load Button");
+    }
+}
