f5e0b7eff493abe3046d2b5b9785396bcc7d428d
Jun Katagiri <katagiri.jun2@gmail.com>
2019-04-10 00:36:35 +1000

    modified hash to sender name in message view

--- Unity/Assets/Scripts/PrefabControllers/MessagePrefabController.cs
+++ Unity/Assets/Scripts/PrefabControllers/MessagePrefabController.cs
@@ -59,7 +59,7 @@ public class MessagePrefabController : MonoBehaviour
     private void Initialize(MessageModel messageModel)
     {
         this.MessageModel = messageModel;
-        this.messageHashText.SetText(messageModel.Hash);
+        this.messageHashText.SetText(messageModel.SenderName);
     }
     
 }
